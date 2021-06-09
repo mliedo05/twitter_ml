4ff814180a1a7af4f835387a3b1aa83ad181e0e9
mliedo05 <mliendo05@gmail.com>
2021-06-08 23:45:06 -0400

    add model twitter and model like, second twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def to_s
+    nickname
+  end
 end
